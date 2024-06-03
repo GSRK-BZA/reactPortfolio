359947c4c10b974f9acfbb0fa3330e50414a2e14
GSRK-BZA <gsrkbza@gmail.com>
2024-06-03 09:31:41 +0530

    added offset to scrolling to ensure that display is not lost

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,11 +9,11 @@ function Nav() {
             <div className='Name'>My Portfolio</div>
             <div className='menu'>
                 <ul>
-                    <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-                    <li><Link to="about" smooth={true} duration={500}>About</Link></li>
-                    <li><Link to="experience" smooth={true} duration={500}>Experience</Link></li>
-                    <li><Link to="testimonial" smooth={true} duration={500}>Testimonial</Link></li>
-                    <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+                    <li><Link to="home" smooth={true} duration={500} offset={-70}>Home</Link></li>
+                    <li><Link to="about" smooth={true} duration={500} offset={-70}>About</Link></li>
+                    <li><Link to="experience" smooth={true} duration={500} offset={-70}>Experience</Link></li>
+                    <li><Link to="testimonial" smooth={true} duration={500} offset={-70}>Testimonial</Link></li>
+                    <li><Link to="contact" smooth={true} duration={500} offset={-70}>Contact</Link></li>
                 </ul>
             </div>
         </div>

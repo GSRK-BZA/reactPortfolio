27ef653b5a6b874de4dd3b2fbeb3bc9f99105747
GSRK-BZA <gsrkbza@gmail.com>
2024-06-06 17:24:53 +0530

    links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,9 @@ const Footer = () => {
       <p>Copyright © 2099 I am Siva Rama Krishna | Design: <a href="https://www.apple.in" target='_blank '>apple is superior</a></p>
       <div className="social-icons">
         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
-        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
-        <a href="https://plus.google.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-google-plus-g"></i></a>
+        <a href="https://x.com/PixelPaneer" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
         <a href="https://www.dribbble.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-dribbble"></i></a>
-        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+        <a href="https://www.linkedin.com/in/siva-rama-krishna-gogineni-2a688a281/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
       </div>
       
     </footer>
